efe05045b53e4fc011975ceb55daa25de07a783c
billpratt <wpratt83@gmail.com>
2018-04-08 18:55:30 -0400

    Added ability to specify number of results and specify output format

---
+++ src/DotNetSearch/Extensions/IntegerFormatExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace DotNetSearch.Extensions
+{
+    public static class IntegerFormatExtensions
+    {
+        public static string ToAbbrString(this int value)
+        {
+            switch(value)
+            {
+                case var exp when (value > 999999):
+                    return exp.ToString("0,,.#0M", CultureInfo.InvariantCulture);
+                case var exp when (value > 999):
+                    return exp.ToString("0,.#0K", CultureInfo.InvariantCulture);
+                default:
+                    return value.ToString();
+            }
+        }
+    }
+}
--- src/DotNetSearch/SearchCommand.cs
+++ src/DotNetSearch/SearchCommand.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
+using DotNetSearch.Extensions;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace DotNetSearch
@@ -19,7 +20,7 @@ namespace DotNetSearch
     class SearchCommand
     {
         private static int MaxOutputStringLength = 20;
-        private static int TakeDefault = 20;
+        private static int TakeDefault = 10;
 
         [Required]
         [Argument(0, "query", Description = "The search terms used to find packages")]
@@ -28,19 +29,47 @@ namespace DotNetSearch
         [Option("--include-prerelease", "Include prerelease packages", CommandOptionType.NoValue)]
         public bool IncludePrerelease { get; set; }
 
-        public int Take { get; set; } = TakeDefault;
+        [Option("-n|--num-results", "Number of results to display. (Default: 10)", CommandOptionType.SingleValue)]
+        public int NumberOfResults { get; set; } = TakeDefault;
 
+        [Option("-o|--output", "One of: table | full", CommandOptionType.SingleValue)]
+        public string Output { get; set; } = "";
+        
         private async Task OnExecuteAsync()
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync($"https://api-v2v3search-0.nuget.org/query?q={Query}&prerelease={IncludePrerelease}&take={Take}");
+            var url = $"https://api-v2v3search-0.nuget.org/query?q={Query}&prerelease={IncludePrerelease}&take={NumberOfResults}";
+            var response = await httpClient.GetAsync(url);
             var searchResponse = await response.Content.ReadAsAsync<SearchResponse>();
 
             if (!response.IsSuccessStatusCode)
             {
                 Console.WriteLine($"No results found for {Query}");
+                return;
             }
 
+            PrintResults(searchResponse);
+        }
+
+        private void PrintResults(SearchResponse searchResponse)
+        {
+            switch(Output)
+            {
+                case var o when (Output.Equals("full", StringComparison.InvariantCultureIgnoreCase)):
+                    PrintFullDetails(searchResponse);
+                    break;
+                default:
+                    PrintTable(searchResponse);
+                    break;
+
+            }
+
+            var numResultsToDisplay = NumberOfResults < searchResponse.TotalHits ? NumberOfResults : searchResponse.TotalHits;
+            Console.WriteLine($"{numResultsToDisplay} of {searchResponse.TotalHits} results found");
+        }
+
+        private void PrintTable(SearchResponse searchResponse)
+        {
             TableFormatter.Print(searchResponse.Data, "No results found", '_', new Dictionary<string, Func<Package, object>>
             {
                 { "Name", x => x.Id},
@@ -53,11 +82,27 @@ namespace DotNetSearch
                             ? $"{joined.Substring(0, MaxOutputStringLength - 1)}..."
                             : joined;
                 }},
-                { "Current Version", x => x.Version},
-                { "Total Downloads", x => x.TotalDownloads.ToString("#,#", CultureInfo.InvariantCulture) },
+                { "Version", x => x.Version},
+                { "Downloads", x => x.TotalDownloads.ToAbbrString() },
                 { "Verified", x => x.Verified ? "    *" : "" }
             });
         }
+
+        private void PrintFullDetails(SearchResponse searchResponse)
+        {
+            foreach (var data in searchResponse.Data)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(data.Id);
+                Console.ResetColor();
+                Console.WriteLine($" by {string.Join(",", data.Authors)}\tv{data.Version}");
+                Console.WriteLine($"{data.Description}{Environment.NewLine}");
+                Console.WriteLine($"Tags: {string.Join(',', data.Tags)}");
+                Console.WriteLine($"Downloads: {data.TotalDownloads.ToAbbrString()}");
+                Console.WriteLine($"{Environment.NewLine}----------------------{Environment.NewLine}");
+            }
+        }
+
         private static string GetVersion()
                     => typeof(SearchCommand).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
     }
